dde5302acb2290f1ef56767280d12ea57bb03337
Corwin <wenightfall@gmail.com>
2023-01-31 19:58:46 +0000

    Completed Lab 2 and changed the todo list to a checkbox.

--- javascript/form.js
+++ javascript/form.js
@@ -69,3 +69,45 @@ function showOutput(event)
 		document.getElementById("output").innerHTML = output;
 	});
 }
+
+function quiz1(event)
+{
+	let number1 = parseInt(prompt("Enter the first number:"));
+	let number2 = parseInt(prompt("Enter the second number:"));
+	let result = number1 * number2;
+	document.getElementById("output").innerHTML = number1 + "*" + number2 + " = " + result;
+	document.getElementById("output").style.color = "red";
+}
+
+function lab2(event)
+{
+	//TASK 1: Display the url of the current webpage
+	document.getElementById("output").innerHTML = "URL:" + "<br>" + document.URL;
+
+
+	//TASK 2: Display the largest number of a user created array
+	let arrSize = parseInt(prompt("Enter size of your desired array"));
+	let maxNum = -Infinity;
+	const arr = new Array(arrSize);
+
+	for(let i = 0; i < arr.length; i++)
+	{
+		arr[i] = parseInt(prompt("Enter int for arr["+i+"]"));
+		if(arr[i] > maxNum)
+		{
+			maxNum = arr[i];
+		}
+
+	}
+	document.getElementById("output").innerHTML += "<br><br>Max number of your arr [" + arr.toString() + "]:<br>" + maxNum;
+
+
+	//TASK 3: Display a user created string in all caps
+	let string = prompt("Enter some text");
+	upperString = string.toUpperCase();
+	document.getElementById("output").innerHTML += "<br><br>string \"" + string + "\" in caps:<br>" + upperString;
+}
+
+function quiz2(event)
+{
+}
